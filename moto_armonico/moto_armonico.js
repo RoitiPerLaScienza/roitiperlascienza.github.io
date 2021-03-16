2d9fbb5127ef8cb371caae957effeb3d0f87ac85
RoitiPerLaScienza <80755515+RoitiPerLaScienza@users.noreply.github.com>
2021-03-16 12:47:11 +0100

    Update moto_armonico.js

--- moto_armonico/moto_armonico.js
+++ moto_armonico/moto_armonico.js
@@ -70,16 +70,16 @@ function preload(){
     grafico = false;
     accelerazione = false;
     peso = false;
-    TestoSpiegazione=loadImage("data/SfondoArmonico.jpg");
-    Simulazione1=loadImage("data/Armonico3.jpg");
-    Simulazione2=loadImage("data/Pendolo.jpg");
-    Pausa=loadImage("data/TastoPausa.png");
-    Play=loadImage("data/TastoPlay.png");
-    Play2=loadImage("data/TastoPlay2.png");
-    Pausa2=loadImage("data/TastoPausa2.png");
-    FormuleOndaArmonica=loadImage("data/FormuleMotoArmonico.PNG");
-    FormulePendolo=loadImage("data/FormulePendolo.PNG");
-    Comicfont=loadFont("data/ComicSansMSBoldItalic.vlw");
+    TestoSpiegazione=loadImage("moto_armonico/data/SfondoArmonico.jpg");
+    Simulazione1=loadImage("moto_armonico/data/Armonico3.jpg");
+    Simulazione2=loadImage("moto_armonico/data/Pendolo.jpg");
+    Pausa=loadImage("moto_armonico/data/TastoPausa.png");
+    Play=loadImage("moto_armonico/data/TastoPlay.png");
+    Play2=loadImage("moto_armonico/data/TastoPlay2.png");
+    Pausa2=loadImage("moto_armonico/data/TastoPausa2.png");
+    FormuleOndaArmonica=loadImage("moto_armonico/data/FormuleMotoArmonico.PNG");
+    FormulePendolo=loadImage("moto_armonico/data/FormulePendolo.PNG");
+    Comicfont=loadFont("moto_armonico/data/ComicSansMSBoldItalic.vlw");
 }
 
 function setup(){
